6a094e097e3fcd76116f988796645e3045fb15fa
CHN-STUDENT <CHN-STUDENT@outlook.com>
2018-10-20 18:45:14 +0800

    Add files via upload

--- REMOTE/main.sh
+++ REMOTE/main.sh
@@ -23,5 +23,5 @@ else
 SFTPPASSWORD
     sed -i '2i REMOTE_DIR="'${REMOTE_DIR}'"' run.sh
     ${sshpass_path} -p ${REMOTE_PASSWD} ssh -o StrictHostKeyChecking=no ${REMOTE_USER}@${REMOTE_IP} -p ${REMOTE_PORT} 'bash -s' < run.sh
-    sed -i '/REMOTE_DIR=*/d' run.sh
+    sed -i '2d' run.sh
 fi
